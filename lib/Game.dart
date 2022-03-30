2a63e8d6f6307fa4b8e9b5d463881a259d7c663c
billz <billzhao1030@gmail.com>
2022-03-30 11:30:31 +1100

    Add firebase config

---
+++ lib/Game.dart
@@ -0,0 +1,15 @@
+
+class Game {
+  String? id;
+
+  String? startTime;
+  String? endTime;
+
+  bool? gameType;
+  bool? gameMode;
+  bool? completed;
+
+  int? repetition;
+
+  //TODO buttonList
+}
